3730966d697581784b477b11df2be9eba534d840
Arshad Shirgave <97735833+arshu78@users.noreply.github.com>
2023-10-06 20:29:12 +0530

    Delete src/store directory

--- src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-import HomeSlice from "./HomeSlice";
-
-export const store = configureStore({
-    reducer: {
-        home: HomeSlice,
-    },
-});
