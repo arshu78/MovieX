0f90ece1fea43db9b9a8ec3f8d1dd03810884033
arshad <arshadshirgave@gmail.com>
2023-10-07 10:51:09 +0530

    store fixed

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,7 @@ function App() {
 
     return (
         <BrowserRouter>
-            
+            <Header/>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/:mediaType/:id" element={<Details />} />
@@ -65,7 +65,7 @@ function App() {
                 <Route path="/explore/:mediaType" element={<Explore />} />
                 <Route path="*" element={<PageNotFound />} />
             </Routes>
-          
+          <Footer/>
         </BrowserRouter>
     );
 }
